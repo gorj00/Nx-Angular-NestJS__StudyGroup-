abe4d2b00592ad21085fb8b3bbbf0d28724f358b
Jan Gorol <jan.gorol@zentity.com>
2020-05-10 00:38:13 +0200

    removing deleted imports

--- apps/backend/src/app/app.service.ts
+++ apps/backend/src/app/app.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { Message } from '@study-group/api-interfaces';
 
 @Injectable()
 export class AppService {
-  getData(): Message {
+  getData(): any {
     return { message: 'Welcome to api!' };
   }
 }
