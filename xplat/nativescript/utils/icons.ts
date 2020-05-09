b6d4d26e4a7d6638c428a032dfb487ea1ccff5d0
Jan Gorol <jan.gorol@zentity.com>
2020-05-09 23:37:12 +0200

    removing xplat folder

--- xplat/nativescript/utils/icons.ts
+++
@@ -1,9 +0,0 @@
-import { isIOS } from '@nativescript/core';
-
-export const backIcon = function() {
-  return `fa-${isIOS ? 'chevron-left' : 'arrow-left'}`;
-};
-
-export const moreIcon = function() {
-  return `fa-${isIOS ? 'ellipsis-h' : 'ellipsis-v'}`;
-};
