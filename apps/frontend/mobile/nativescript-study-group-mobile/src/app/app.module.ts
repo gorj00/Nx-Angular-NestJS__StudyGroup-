adb741c6179a4934d3fe553a0207b53366fd839b
Jan Gorol <jan.gorol@zentity.com>
2020-05-10 00:24:09 +0200

    adding ns app and removing root apps

---
+++ apps/frontend/mobile/nativescript-study-group-mobile/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { NativeScriptModule, NativeScriptHttpClientModule } from '@nativescript/angular';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ItemsComponent } from './item/items.component';
+import { ItemDetailComponent } from './item/item-detail.component';
+
+@NgModule({
+  bootstrap: [AppComponent],
+  imports: [NativeScriptModule, NativeScriptHttpClientModule, AppRoutingModule],
+  declarations: [AppComponent, ItemsComponent, ItemDetailComponent],
+  providers: [],
+  schemas: [NO_ERRORS_SCHEMA]
+})
+/*
+Pass your application module to the bootstrapModule function located in main.ts to start your app
+*/
+export class AppModule {}
